cd77fe776289410030d3fe402859e8267d58c560
Jessica Brazelton <jbrazelt@thoughtworks.com>
2016-02-26 20:43:03 -0600

    Added button to get speech on click.

--- wear/src/main/java/com/hacked/MainActivity.java
+++ wear/src/main/java/com/hacked/MainActivity.java
@@ -1,24 +1,57 @@
 package com.hacked;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.wearable.view.WatchViewStub;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.Optional;
 
 public class MainActivity extends Activity {
 
-    private TextView mTextView;
+    private static final int SPEECH_REQUEST_CODE = 0;
+    private Button addReminderBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+                addReminderBtn = (Button) stub.findViewById(R.id.add_reminder_btn);
+
+                addReminderBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                        startActivityForResult(intent, SPEECH_REQUEST_CODE);
+                    }
+                });
+
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
